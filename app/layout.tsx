f4674ec20a41074974ca2bb727da4adb5d61438c
Deepak-5431 <deepakchand540@gmail.com>
2026-05-01 09:54:19 +0530

    again title in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
  // title: "Gradeplus - Transforming Your School",
  // description: "Gradeplus app.com",
   metadataBase: new URL('https://gradeplusapp.com'),
-  title: "Gradeplus - Transforming Your School",
-  description: 'The complete management solution for schools and institutes.',
+  title: "Gradeplus | AI-Powered Learning",
+  description: 'Stuck on a problem? Simply snap a picture. GradePlus uses advanced AI tools to help students solve questions instantly and empower educators.',
   authors: [{ name: 'GradePlus Team', url: 'https://gradeplusapp.com' }],
   publisher: 'IBLIB Educations',
   verification: {
